559888a565f076e17b245d33af7cdcffe4791ff3
Маргарита Лукина <mlukina@Mbp-mlukina.local>
2022-08-20 17:48:37 +0300

    fix header

--- src/atoms/Header/Header.tsx
+++ src/atoms/Header/Header.tsx
@@ -1,7 +1,8 @@
 import React from "react";
+import styles from "./Header.module.css";
 
 interface IHeaderProps {
     text:string;
 }
 
-export const Header:React.FC<IHeaderProps> = ({ text }:IHeaderProps) => (<h3>{text}</h3>)
+export const Header:React.FC<IHeaderProps> = ({ text }:IHeaderProps) => (<h3 className={styles.header}>{text}</h3>)
