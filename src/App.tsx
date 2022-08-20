ea4f09ed8fa7152da94983c2d6aef730ae8592f5
Маргарита Лукина <mlukina@Mbp-mlukina.local>
2022-08-20 17:10:35 +0300

    add atoms

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,12 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import {Input} from "./atoms/Input/Input";
 
 function App() {
+  const [name, setName] = React.useState<string>("");
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <Input value={name} setValue={setName}/>
     </div>
   );
 }
---
+++ src/atoms/Input/Input.tsx
@@ -0,0 +1,16 @@
+import React, {ChangeEvent} from "react";
+import styles from "./Input.module.css";
+
+interface IInputProps {
+    value:string;
+    setValue(newValue:string):void;
+}
+
+export const Input:React.FC<IInputProps> = ({value, setValue}:IInputProps) => (
+    <input
+        className={styles.input}
+        type="text"
+        onChange={(e:ChangeEvent<HTMLInputElement>) => setValue(e.target.value)}
+        value={value}
+    />
+)
